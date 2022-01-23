1d2efc566453c09591387b4db9401ea42b11dd7e
imsahiljain <emailofsahiljain@gmail.com>
2022-01-23 18:48:23 +0530

    feat: uppercase text

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/TextArea.css';
 
 const Header = () => {
-  return (
-    <div className="textarea">
-        <p>Enter text here:</p>
-        <textarea name="" id="" cols="60" rows="17"></textarea>
-    </div>
-  );
+    const uppercaseText = () => {
+        let newText = text.toUpperCase();
+        updateText(newText);
+    }
+
+    const onchangeText = (event) => {
+        updateText(event.target.value);
+    }
+
+    const [text, updateText] = useState("");
+    return (
+        <div className="textarea">
+            <p>Enter text here:</p>
+            <textarea name="" id="" cols="60" rows="17" value={text} onChange={onchangeText} placeholder='Enter text here'></textarea>
+            <br /><br />
+            <button className="btn btn-primary" onClick={uppercaseText}>Uppercase</button>
+        </div>
+    );
 };
 
 export default Header;

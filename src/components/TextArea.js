9532cecec85bad3120558fa6fb2fe7ee857fff63
imsahiljain <emailofsahiljain@gmail.com>
2022-01-23 21:10:25 +0530

    add: more func.

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -12,6 +12,17 @@ const Header = () => {
         updateText(newText);
     }
 
+    const clearText = () => {
+        let newText = "";
+        updateText(newText);
+    }
+
+    const copyText = () => {
+        var text = document.getElementById("textarea")
+        text.select();
+        navigator.clipboard.writeText(text.value)
+    }
+
     const onchangeText = (event) => {
         updateText(event.target.value);
     }
@@ -21,14 +32,15 @@ const Header = () => {
         <>
             <div className="textarea">
                 <p>Enter text here:</p>
-                <textarea name="" id="" cols="60" rows="17" value={text} onChange={onchangeText} placeholder='Enter text here'></textarea>
+                <textarea id="textarea" cols="60" rows="17" value={text} onChange={onchangeText} placeholder='Enter text here'></textarea>
                 <br /><br />
-                <button className="btn btn-primary" onClick={uppercaseText}>Uppercase</button>
-                <button className="btn btn-primary" onClick={lowercaseText}>Lowercase</button>
+                <button className="btn btn-blue" onClick={uppercaseText}>Uppercase</button>
+                <button className="btn btn-blue" onClick={lowercaseText}>Lowercase</button>
+                <button className="btn btn-red" onClick={clearText}>Clear</button>
+                <button className="btn btn-green" onClick={copyText}><ion-icon name="copy-outline"></ion-icon></button>
             </div>
 
             <div className="textdetails">
-                <h2>Text Details</h2>
                 <p><b>Words: </b> {text.split(" ").length} </p>
                 <p><b>Charecters: </b> {text.length} </p>
             </div>

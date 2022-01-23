4c2d0b89a00a9a73b73e2b74ca301f01af2147ba
imsahiljain <emailofsahiljain@gmail.com>
2022-01-23 17:57:16 +0530

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Header.css';
+
+const Header = () => {
+  return (
+    <div className="heading">
+        <h1>text.io</h1>
+        <p id="source">Do anything with your text.</p>
+        <hr/>
+    </div>
+  );
+};
+
+export default Header;
